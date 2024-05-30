68fc09a6eee768d48626aeff7e308e7b765ed9ea
truthless-dev <truthless.deviant248@simplelogin.fr>
2024-05-30 15:31:01 -0700

    Add getters and queriers to model classes

--- simplelogincmd/database/models.py
+++ simplelogincmd/database/models.py
@@ -59,6 +59,108 @@ class Object(DeclarativeBase):
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.id})"
 
+    @classmethod
+    def identifier_query(cls, query: Select, id: Any) -> Select:
+        """
+        Restrict a query based on a generic identifier
+
+        In the event that `id` is not a numeric primary key id, model
+        classes can decide for themselves, by overriding this method,
+        how to find the objects to which `id` refers. Overriding
+        implementations should restrict `query`, as via a call to
+        :meth:`sqlalchemy.Select.where` or similar, to a condition that
+        identifies one (ideally) or more objects. For example, a
+        Mailbox model might allow to search through email addresses,
+        while an Alias model might search emails and alias notes for
+        a match.
+
+        :param query: The query to be conditioned
+        :type query: :class:`sqlalchemy.Select`
+        :param id: The identifier which is supposed to identify ideally
+            one, but potentialy many or no model objects
+        :type id: Any
+
+        :return: The modified query
+        :rtype: :class:`sqlalchemy.Select`
+        """
+        return query
+
+    @classmethod
+    def resolve_identifier(cls, session: Session, id: Any) -> list["Object"]:
+        """
+        Retrieve a list of model objects based on a generic identifier
+
+        Attempt to produce a single model object by passing `id`
+        directly on to :meth:`sqlalchemy.orm.Session.get_one`. If
+        that fails, then fall back to
+        :meth:`~simplelogincmd.database.models.identifier_query`, which
+        subclasses can override in order to determine how that
+        particular model should interpret the identifier.
+
+        :param session: The database session which is to search for
+            object(s)
+        :type session: :class:`sqlalchemy.orm.Session`
+        :param id: The identifier for which to search. This might be
+            as simple as a primary key numeric id, or it could be a
+            string which (ideally uniquely) identifies a particular
+            model object.
+        :type id: Any
+
+        :return: A list of zero or more matching model objects
+        :rtype: list[Object]
+        """
+        try:
+            return [session.get_one(cls, id)]
+        # Catch bad `id` value as well as no result found.
+        except InvalidRequestError:
+            query = select(cls)
+            query = cls.identifier_query(query, id)
+            return session.scalars(query).all()
+
+    def get(self, field: str) -> str | None:
+        """
+        Retrieve the value of the given field
+
+        :param field: The name of the field to get
+        :type field: str
+
+        :return: The field's value, or None if the field is undefined
+        :rtype: str | None
+        """
+        return getattr(self, field, None)
+
+    def get_string(self, field: str) -> str:
+        """
+        Retrieve a string representation of the field
+
+        :param field: The name of the field to get
+        :type field: str
+
+        :return: String representation of the field's value
+
+            - if value is string, then return value
+            - if value is None, then ""
+            - if value is bool, then "Y" or "N"
+            - if value is iterable, then each element as a string,
+              separated by commas
+            - otherwise return str(value)
+
+        :rtype: str
+        """
+        value = self.get(field)
+        if isinstance(value, str):
+            return value
+        if value is None:
+            return ""
+        if isinstance(value, bool):
+            return "Y" if value else "N"
+        if not isinstance(value, dict):
+            try:
+                return ", ".join(str(elem) for elem in value)
+            except TypeError:
+                pass
+        return str(value)
+
 
 class Mailbox(LenientInit, Object):
     """
@@ -79,6 +181,12 @@ class Mailbox(LenientInit, Object):
 
     def __str__(self) -> str:
         return self.email
+
+    @classmethod
+    def identifier_query(cls, query: Select, id: Any) -> Select:
+        like_string = f"%{id}%"
+        email_like = cls.email.ilike(like_string)
+        return query.where(email_like)
 
 
 class Alias(LenientInit, Object):
@@ -116,3 +224,11 @@ class Alias(LenientInit, Object):
 
     def __str__(self) -> str:
         return self.email
+
+    @classmethod
+    def identifier_query(cls, query: Select, id: Any) -> Select:
+        like_string = f"%{id}%"
+        email_like = cls.email.ilike(like_string)
+        note_like = cls.note.ilike(like_string)
+        condition = email_like | note_like
+        return query.where(condition)
